4a9e5825ff07b2e4d34a820ce55760f8eff822bd
VikasNagare45 <vikasnagare05@gmail.com>
2019-04-24 11:13:43 +0530

    Create TeacherService.java

---
+++ DocumentaryPortal/src/main/java/com/vidnyan/DocumertyPortal/servise/TeacherService.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package com.vidnyan.DocumertyPortal.servise;
+
+import java.io.File;
+
+//import com.vidnyan.DocumertyPortal.doa.TeacherDoa;
+import com.vidnyan.DocumertyPortal.model.Teacher;
+/**
+ * @author vikasnagare
+ *
+ */
+public class TeacherService {
+
+	/**
+	 * 
+	 */
+	public TeacherService() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void createTeacher(String id) {
+		String x="//Users//vikasnagare//Desktop//MCA//SEM 4//AVDJAVA//DocumertyPortal//src//main//webapp//users//";
+		new File(x+id);
+	}
+
+	
+}
